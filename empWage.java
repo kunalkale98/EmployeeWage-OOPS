a5bde6d5ff71b7a8255d3c14d40ebb2d64a861ad
kunalkale98 <kunal,k2898@gmail.com>
2021-02-23 20:54:05 +0530

    Added: Emp Wage using Switch Case

--- empWage.java
+++ empWage.java
@@ -3,26 +3,38 @@ class empWage{
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	public void attendance(int wage_Per_Hr, int full_Day_Hrs, int part_Time_Hrs){
+	public int attendance(int wage_Per_Hr){
 		int check = (int)Math.floor(Math.random() * 10) % 3;
-		if(check==IS_FULL_TIME){
+		int work_Hrs;
+		switch (check){
+
+		case 1:
 			System.out.println("Employee is Present Full Time");
-			int emp_Wage = wage_Per_Hr * full_Day_Hrs;
-			System.out.println("Full Time Employee Wage is "+emp_Wage);
-		}
-		else if(check==IS_PART_TIME){
-			System.out.println("Employee is Part Time");
-			int emp_Wage = wage_Per_Hr * part_Time_Hrs;
-			System.out.println("Part Time Employee Wage is "+emp_Wage);
-		}
-		else{
+			work_Hrs = 8;
+			break;
+
+		case 2:
+			System.out.println("Employee is Present Part Time");
+			work_Hrs = 4;
+			break;
+
+		default:
 			System.out.println("Employee is Absent");
+			work_Hrs = 0;
 		}
+
+		int emp_Wage = wage_Per_Hr * work_Hrs;
+		return emp_Wage;
+      //System.out.println("Employee Wage is "+emp_Wage);
 	}
+
 
 	public static void main(String[] args){
 		empWage emp = new empWage();
-		emp.attendance(20,8,4);
+		int emp_Wage_1 = emp.attendance(20);
+		System.out.println("Employee 1 Wage is "+emp_Wage_1);
+		int emp_Wage_2 = emp.attendance(25);
+		System.out.println("Employee 2 Wage is "+emp_Wage_2);
 	}
 
 }
