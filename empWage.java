0873ce9024f8069756e788de63c250d78a3cca1e
kunalkale98 <kunal,k2898@gmail.com>
2021-02-23 18:40:04 +0530

    Added: Daily Employee Wage

--- empWage.java
+++ empWage.java
@@ -1,11 +1,14 @@
 class empWage{
 
-	public static final int isPresent = 1;
+	public static final int IS_FULL_TIME = 1;
 
-	public void attendance(){
-		int check = (int)Math.floor((Math.random()*10)%2);
-		if(check == isPresent){
+	public void attendance(int wage_Per_Hr, int full_Day_Hrs){
+		int check = (int)Math.floor(Math.random() * 10) % 2;
+
+		if(check==IS_FULL_TIME){
 			System.out.println("Employee is Present");
+			int emp_Wage = wage_Per_Hr * full_Day_Hrs;
+			System.out.println("Daily Employee Wage is "+emp_Wage);
 		}
 		else{
 			System.out.println("Employee is Absent");
@@ -13,8 +16,9 @@ class empWage{
 	}
 
 
+
 	public static void main(String[] args){
 		empWage emp = new empWage();
-		emp.attendance();
+		emp.attendance(20,8);
 	}
 }
